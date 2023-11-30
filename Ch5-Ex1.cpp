0d18f4245cd63f1016720824cda2d2c72fe54d4a
HeatherKolinskyBSU <146090360+HeatherKolinskyBSU@users.noreply.github.com>
2023-11-30 16:29:32 +0000

    Ch5-Ex1
    Exercise 1 from Chapter 5.
    Should work.
    Has comments.

---
+++ Ch5-Ex1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+void hello() {               // Declares the function
+	cout << "Hello" << endl;
+}
+
+int main() {                 // Begins the "main" function
+	hello();                 // Calls the "hello" function
+
+	return 0;
+}                            // Ends "main" function
