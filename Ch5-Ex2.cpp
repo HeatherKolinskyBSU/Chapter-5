6f5424f0e3e56a48e30f2c8fadb218ff9b104699
HeatherKolinskyBSU <146090360+HeatherKolinskyBSU@users.noreply.github.com>
2023-12-07 17:38:42 +0000

    Ch5-Ex2_Calculator_FINAL_07/12/2023
    Fully working calculator program with annotated code.
    Includes input validation and replay loops.

--- Ch5-Ex2.cpp
+++ Ch5-Ex2.cpp
@@ -5,59 +5,95 @@ void userCalculationAddition(double userNum, double userNum2){
 	double total = userNum + userNum2;
 	cout << total << endl;
 }
+// Stores the code which calculates the addition of two numbers
+// Will output 'total' to screen when called by Main
+
 void userCalculationSubtraction(double userNum, double userNum2) {
 	double total = userNum - userNum2;
 	cout << total << endl;
 }
+// Stores the code which calculates the subtraction of two numbers
+// Will output 'total' to screen when called by Main
+
 void userCalculationMultiplication(double userNum, double userNum2) {
 	double total = userNum * userNum2;
 	cout << total << endl;
 }
+// Stores the code which calculates the Multiplication of two numbers
+// Will output 'total' to screen when called by Main
+
 void userCalculationDivision(double userNum, double userNum2) {
 	double total = userNum / userNum2;
 	cout << total << endl;
 }
+// Stores the code which calculates the Division of two numbers
+// Will output 'total' to screen when called by Main
 
-int main()
+int main() // Starts Main (function)
 {
-	double userNum;
-	cout << "Enter the first number:  ";
-	cin >> userNum;
-	double userNum2;
-	cout << "Enter the second number: ";
-	cin >> userNum2;
+	while (true) { // Loops the program so you do not have
+		       // to restart to do every calculation
 
-	cout << endl;
+		double userNum;
+		cout << "Enter the first number:  ";
+		cin >> userNum;
+		// Creates the variable 'userNum' to store a number with a floating point
+		// Program asks user for an input and then waits for one
+		double userNum2;
+		cout << "Enter the second number: ";
+		cin >> userNum2;
+		// Creates the variable 'userNum2' to store a number with a floating point
+                // Program asks user for an input and then waits for one
 
-	string calculation;
-	char userInput;
-	cout << "What would you like to do with these numbers?" << endl;
-	cout << "Addition       | +" << endl;
-	cout << "Subtraction    | -" << endl;
-	cout << "Multiplication | *" << endl;
-	cout << "Division       | /" << endl;
-	cout << "(Enter the operation symbol noted)" << endl;
-	cin >> userInput;
+		cout << endl;
 
-	cout << endl;
+		string calculation;
+		char userInput;
+		cout << "What would you like to do with these numbers?" << endl;
+		cout << "Addition       | +" << endl;
+		cout << "Subtraction    | -" << endl;
+		cout << "Multiplication | *" << endl;
+		cout << "Division       | /" << endl;
+		cout << "(Enter the operation symbol noted)" << endl;
+		cin >> userInput;
+		// Creates a character imput and waits for one.
+		// Gives the user 4 options as to what the program does
 
-	if (userInput == '+') {
-		userCalculationAddition(userNum, userNum2);
-		return 0;
+		cout << endl;
+
+		if (userInput == '+') {
+			userCalculationAddition(userNum, userNum2);
+		}
+		// Calls the void for addition
+
+		else if (userInput == '-') {
+			userCalculationSubtraction(userNum, userNum2);
+		}
+		// Calls the void for subtraction
+
+		else if (userInput == '*') {
+			userCalculationMultiplication(userNum, userNum2);
+		}
+		// Calls the void for multiplication
+
+		else if (userInput == '/') {
+			userCalculationDivision(userNum, userNum2);
+		}
+		// Calls the void for division
+
+		else cout << "Invalid Input." << endl;
+		// Ignores any other input and continues the program
+
+		char answer;
+		cout << "Enter another calculation?" << endl;
+		cout << "('n' or 'N' exits program)" << endl;
+		// Asks user if they want to use the program again
+		cin >> answer;
+		if (answer == 'n' || answer == 'N') {
+			break;
+		}
+		// If the user answers 'n' or 'N', the program ends.
+		// Any other input will run the program from the beginning
 	}
-	else if (userInput == '-') {
-		userCalculationSubtraction(userNum, userNum2);
-		return 0;
-	}
-	else if (userInput == '*') {
-		userCalculationMultiplication(userNum, userNum2);
-		return 0;
-	}
-	else if (userInput == '/') {
-		userCalculationDivision(userNum, userNum2);
-		return 0;
-	}
-	else cout << "Invalid Input." << endl;
-
 	return 0;
-}
+} // Ends Main (function)
